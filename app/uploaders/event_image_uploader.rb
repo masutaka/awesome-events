a0eb700d8372320f713fae2dfd16eafadf0c0f1c
Takashi Masuda <masutaka.net@gmail.com>
2014-07-10 22:20:58 +0900

    Add feature resizes an image

--- app/uploaders/event_image_uploader.rb
+++ app/uploaders/event_image_uploader.rb
@@ -4,7 +4,7 @@ class EventImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,6 +23,8 @@ class EventImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+
+  process :resize_to_limit => [1130, 1130]
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
